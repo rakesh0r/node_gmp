5793d9a415d00676357d096823a83f7fcf73b402
Rakesh Rekala <rakeshrekala@Rakeshs-MacBook-Pro.local>
2021-03-26 16:26:22 +0530

    es lint addedd

--- module1/task3.js
+++ module1/task3.js
@@ -2,8 +2,8 @@ import csv from 'csvtojson';
 import { createWriteStream, createReadStream } from 'fs';
 import path from 'path';
 
-const csvFilePath=path.resolve(__dirname, "..", "hw1/csv/hw1.csv");
-const txtFilePath=path.resolve(__dirname, "..", "hw1/csv/hw1.txt");
+const csvFilePath=path.resolve(__dirname, "..", "module1/csv/hw1.csv");
+const txtFilePath=path.resolve(__dirname, "..", "module1/csv/hw1.txt");
 
 const writeStream = createWriteStream(txtFilePath);
 
